481ad7ce6198b0f3204d6715935e28c2f5ae491c
落心 <luoxin.ttt@gmail.com>
2021-01-28 10:46:27 +0800

    api

--- api.go
+++ api.go
@@ -124,13 +124,13 @@ func GetUsableNodeList() ([]*subscription.ProxyNode, error) {
 	err := s.Db.Where("is_close = ?", false).
 		Where("proxy_speed >= 0 ").
 		// Where("proxy_node_type = 1").
-		Where("available_count >= 0 ").
-		Where("proxy_network_delay >= 0").
+		Where("available_count > 0 ").
+		Where("proxy_network_delay > 0").
 		Where("death_count < ?", 10).
 		// Order("proxy_node_type").
 		Order("available_count DESC").
-		Order("proxy_speed DESC").
-		Order("proxy_network_delay DESC").
+		// Order("proxy_speed DESC").
+		Order("proxy_network_delay").
 		Order("death_count").
 		Order("last_crawler_at DESC").
 		Limit(50).
