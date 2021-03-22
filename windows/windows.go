92991010e911e6671cc221a7a8bd4f3cb9f28b39
落心 <luoxin.ttt@gmail.com>
2021-03-22 21:04:08 +0800

    fix

---
+++ windows/windows.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	log "github.com/sirupsen/logrus"
+
+	"subscribe"
+)
+
+// netsh interface ipv4 show excludedportrange protocol=tcp
+// https://superuser.com/questions/1486417/unable-to-start-kestrel-getting-an-attempt-was-made-to-access-a-socket-in-a-way
+
+func main() {
+	err := subscribe.Init()
+	if err != nil {
+		log.Errorf("err:%v", err)
+		return
+	}
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+	<-sigCh
+
+	// a := app.New()
+	// w := a.NewWindow("Hello")
+	//
+	// hello := widget.NewLabel("Hello Fyne!")
+	// w.SetContent(container.NewVBox(
+	// 	hello,
+	// 	widget.NewButton("Hi!", func() {
+	// 		hello.SetText("Welcome :)")
+	// 	}),
+	// ))
+
+	// w.ShowAndRun()
+}
