1be74b9f42ab1b26ef8102dad472b941e05b03cc
落心 <luoxin.ttt@gmail.com>
2021-04-20 10:39:26 +0800

    减少日志量

--- task/proxy_check.go
+++ task/proxy_check.go
@@ -61,7 +61,7 @@ func checkProxyNode(check *proxycheck.ProxyCheck) error {
 			um["death_count"] = node.DeathCount
 
 			log.Infof("check proxy %+v: speed:%v Kb/s, delay:%v ms, available %d, death %d",
-				node.UrlFeature, node.ProxySpeed, node.ProxyNetworkDelay, node.AvailableCount, node.DeathCount)
+				utils.ShortStr(node.UrlFeature, 12), node.ProxySpeed, node.ProxyNetworkDelay, node.AvailableCount, node.DeathCount)
 
 			node.NextCheckAt = node.CheckInterval + utils.Now()
 
