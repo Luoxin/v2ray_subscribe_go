cbeff4bc7b2657deb1d0cf700c3d3f05c4868920
落心 <luoxin.ttt@gmail.com>
2021-06-08 15:39:58 +0800

    service

--- dns/dns.go
+++ dns/dns.go
@@ -1,12 +1,7 @@
 package dns
 
 import (
-	"fmt"
 	"net"
-	"os"
-	"os/signal"
-	"strconv"
-	"syscall"
 	"time"
 
 	"github.com/Luoxin/Eutamias/conf"
@@ -15,7 +10,6 @@ import (
 	"github.com/bluele/gcache"
 	"github.com/elliotchance/pie/pie"
 	"github.com/go-ping/ping"
-	"github.com/miekg/dns"
 	"github.com/panjf2000/ants/v2"
 	log "github.com/sirupsen/logrus"
 )
@@ -290,74 +284,4 @@ func LookupHostsFastestIp(domain string) string {
 	} else {
 		return dnsClient.QueryIpv4FastestIp(domain)
 	}
-}
-
-func InitDnsService() error {
-	if !conf.Config.Dns.EnableService {
-		log.Warnf("dns service not start")
-		return nil
-	}
-
-	server := &dns.Server{Addr: ":" + strconv.Itoa(int(conf.Config.Dns.ServicePort)), Net: "udp"}
-	dns.HandleFunc(".", func(w dns.ResponseWriter, r *dns.Msg) {
-		m := new(dns.Msg)
-		m.SetReply(r)
-		m.Compress = false
-
-		switch r.Opcode {
-		case dns.OpcodeQuery:
-			for _, q := range m.Question {
-				switch q.Qtype {
-				case dns.TypeA:
-					log.Debugf("lookup %v", q.Name)
-					// ipList := LookupAllHosts(q.Name)
-					// ipList.Each(func(ip string) {
-					// 	rr, err := dns.NewRR(fmt.Sprintf("%s A %s", q.Name, ip))
-					// 	if err == nil {
-					// 		m.Answer = append(m.Answer, rr)
-					// 	}
-					// 	log.Infof("[dns query]%v %v", q.Name, ip)
-					// })
-
-					ip := LookupHostsFastestBack(q.Name)
-					if ip != "" {
-						rr, err := dns.NewRR(fmt.Sprintf("%s A %s", q.Name, ip))
-						if err == nil {
-							head := rr.Header()
-							head.Ttl = 60
-							m.Answer = append(m.Answer, rr)
-						}
-						log.Infof("[dns query]%v %v", q.Name, ip)
-					}
-				}
-			}
-		default:
-			log.Warnf("unsupported opcode")
-		}
-
-		err := w.WriteMsg(m)
-		if err != nil {
-			log.Errorf("err:%v", err)
-		}
-	})
-
-	sigCh := make(chan os.Signal, 1)
-	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP, syscall.SIGQUIT)
-	go func() {
-		err := server.ListenAndServe()
-		if err != nil {
-			log.Errorf("err:%v", err)
-			return
-		}
-	}()
-	go func() {
-		<-sigCh
-		log.Info("dns service stop")
-		err := server.Shutdown()
-		if err != nil {
-			log.Errorf("err:%v", err)
-		}
-	}()
-
-	return nil
 }
---
+++ dns/service.go
@@ -0,0 +1,83 @@
+package dns
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+
+	"github.com/Luoxin/Eutamias/conf"
+	"github.com/miekg/dns"
+	log "github.com/sirupsen/logrus"
+)
+
+func InitDnsService() error {
+	if !conf.Config.Dns.EnableService {
+		log.Warnf("dns service not start")
+		return nil
+	}
+
+	server := &dns.Server{Addr: ":" + strconv.Itoa(int(conf.Config.Dns.ServicePort)), Net: "udp"}
+	dns.HandleFunc(".", func(w dns.ResponseWriter, r *dns.Msg) {
+		m := new(dns.Msg)
+		m.SetReply(r)
+		m.Compress = false
+
+		switch r.Opcode {
+		case dns.OpcodeQuery:
+			for _, q := range m.Question {
+				switch q.Qtype {
+				case dns.TypeA:
+					log.Debugf("lookup %v", q.Name)
+					// ipList := LookupAllHosts(q.Name)
+					// ipList.Each(func(ip string) {
+					// 	rr, err := dns.NewRR(fmt.Sprintf("%s A %s", q.Name, ip))
+					// 	if err == nil {
+					// 		m.Answer = append(m.Answer, rr)
+					// 	}
+					// 	log.Infof("[dns query]%v %v", q.Name, ip)
+					// })
+
+					ip := LookupHostsFastestBack(q.Name)
+					if ip != "" {
+						rr, err := dns.NewRR(fmt.Sprintf("%s A %s", q.Name, ip))
+						if err == nil {
+							head := rr.Header()
+							head.Ttl = 60
+							m.Answer = append(m.Answer, rr)
+						}
+						log.Infof("[dns query]%v %v", q.Name, ip)
+					}
+				}
+			}
+		default:
+			log.Warnf("unsupported opcode")
+		}
+
+		err := w.WriteMsg(m)
+		if err != nil {
+			log.Errorf("err:%v", err)
+		}
+	})
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP, syscall.SIGQUIT)
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Errorf("err:%v", err)
+			return
+		}
+	}()
+	go func() {
+		<-sigCh
+		log.Info("dns service stop")
+		err := server.Shutdown()
+		if err != nil {
+			log.Errorf("err:%v", err)
+		}
+	}()
+
+	return nil
+}
