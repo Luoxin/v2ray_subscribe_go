ecc0477efb3d05ffd8c6edfb9e2bfb0f875b63c2
落心 <luoxin.ttt@gmail.com>
2021-06-28 10:14:07 +0800

    优化日志输出
    Signed-off-by: 落心 <luoxin.ttt@gmail.com>

--- proxy/parse.go
+++ proxy/parse.go
@@ -23,62 +23,63 @@ func ParseProxy(content string) (p Proxy, err error) {
 	case strings.HasPrefix(content, "ssr://"):
 		p, err = ParseSSRLink(content)
 		if err != nil {
-			log.Errorf("err:%v", err)
+			log.Errorf("parse:%v,err:%v", content, err)
+			return
 		}
 
 	case strings.HasPrefix(content, "vmess://"):
 		p, err = ParseVmessLink(content)
 		if err != nil {
-			log.Errorf("err:%v", err)
+			log.Errorf("parse:%v,err:%v", content, err)
 			return
 		}
 
 	case strings.HasPrefix(content, "ss://"):
 		p, err = ParseSSLink(content)
 		if err != nil {
-			log.Errorf("err:%v", err)
+			log.Errorf("parse:%v,err:%v", content, err)
 			return
 		}
 
 	case strings.HasPrefix(content, "trojan://"):
 		p, err = ParseTrojanLink(content)
 		if err != nil {
-			log.Errorf("err:%v", err)
+			log.Errorf("parse:%v,err:%v", content, err)
 			return
 		}
 
 	case strings.HasPrefix(content, "http://"):
 		p, err = ParseHttpLink(content)
 		if err != nil {
-			log.Errorf("err:%v", err)
+			log.Errorf("parse:%v,err:%v", content, err)
 			return
 		}
 
 	case strings.HasPrefix(content, "https://"):
 		p, err = ParseHttpLink(content)
 		if err != nil {
-			log.Errorf("err:%v", err)
+			log.Errorf("parse:%v,err:%v", content, err)
 			return
 		}
 
 	case strings.HasPrefix(content, "socket://"):
 		p, err = ParseSocketLink(content)
 		if err != nil {
-			log.Errorf("err:%v", err)
+			log.Errorf("parse:%v,err:%v", content, err)
 			return
 		}
 
 	case strings.HasPrefix(content, "socket4://"):
 		p, err = ParseSocketLink(content)
 		if err != nil {
-			log.Errorf("err:%v", err)
+			log.Errorf("parse:%v,err:%v", content, err)
 			return
 		}
 
 	case strings.HasPrefix(content, "socket5://"):
 		p, err = ParseSocketLink(content)
 		if err != nil {
-			log.Errorf("err:%v", err)
+			log.Errorf("parse:%v,err:%v", content, err)
 			return
 		}
 
