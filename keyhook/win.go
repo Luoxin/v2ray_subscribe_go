5c72bd49c6114b99e563abaefba554c5aa28ebcc
落心 <luoxin.ttt@gmail.com>
2021-07-14 23:47:10 +0800

    - 添加对剪切事件的监听 - 添加cache做一小时的内去重 - 添加md5的计算

--- keyhook/win.go
+++ keyhook/win.go
@@ -7,22 +7,38 @@ import (
 	"os/signal"
 	"strings"
 	"syscall"
+	"time"
 
 	"github.com/Luoxin/Eutamias/node"
 	"github.com/Luoxin/Eutamias/parser"
+	"github.com/Luoxin/Eutamias/utils"
 	"github.com/atotto/clipboard"
+	"github.com/bluele/gcache"
 	"github.com/go-vgo/robotgo"
 	"github.com/robotn/gohook"
 	log "github.com/sirupsen/logrus"
 )
 
 func InitKeyHook() error {
+	cache := gcache.New(128).LRU().Build()
 	copyFunc := func(event hook.Event) {
 		content, err := clipboard.ReadAll()
 		if err != nil {
 			log.Errorf("err:%v", err)
 			return
 		}
+
+		_, err = cache.Get(utils.Md5(content))
+		if err == nil {
+			return
+		} else if err == gcache.KeyNotFoundError {
+
+		} else {
+			log.Errorf("err:%v", err)
+			return
+		}
+
+		_ = cache.SetWithExpire(utils.Md5(content), true, time.Hour)
 
 		log.Infof("get %v from clipboard", content)
 
@@ -39,6 +55,7 @@ func InitKeyHook() error {
 
 	robotgo.EventHook(hook.KeyDown, []string{"ctrl", "c", "c"}, copyFunc)
 	robotgo.EventHook(hook.KeyDown, []string{"ctrl", "x"}, copyFunc)
+	robotgo.EventHook(hook.KeyDown, []string{"ctrl", "v"}, copyFunc)
 
 	s := robotgo.EventStart()
 	sigCh := make(chan os.Signal, 1)
